7ec7caed060a5397e81aabe25e4d0035e1de75a4
Amar Khoirudin <amar.khoi25@gmail.com>
2020-09-03 14:23:03 +0700

    Destructing folder

---
+++ src/pages/Splash/index.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('WelcomeAuth');
+    }, 3000);
+  }, []);
+  return (
+    <View>
+      <Text>Splash Screen Page</Text>
+    </View>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({});
